d6a466ad0a0a5dabb5bac27e4100ddce7254f15a
Jiming <jiming.liu2@icloud.com>
2026-04-18 16:40:27 +0700

    Fix: use videoUrl not imageUrl for merge-audio

--- worker/index.js
+++ worker/index.js
@@ -49,7 +49,7 @@ async function pollShots() {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
-            imageUrl: shot.twin_frame_url,
+            videoUrl: shot.twin_frame_url,
             audioUrl: shot.audio_url
           })
         },
